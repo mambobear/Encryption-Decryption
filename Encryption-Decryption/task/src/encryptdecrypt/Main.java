6bb6c7923f2495fa2e1a1b50ecd1ed72e0a7dec6
Andrei Zherebtsov <azherebtsov@gmail.com>
2021-03-03 17:56:30 -0800

    Complete Stage 3

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -4,36 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
+        String op = scanner.nextLine();
         String message = scanner.nextLine();
         int key = scanner.nextInt();
-        System.out.println(encryptCaesar(message, key));
+
+        if ("enc".equals(op)) {
+            System.out.println(encryptCaesar(message, key));
+        } else if ("dec".equals(op)) {
+            System.out.println(encryptCaesar(message, -key));
+        }
     }
 
     private static String encryptCaesar(String message, int key) {
+        int SIZE = 256;
+
         StringBuilder strb = new StringBuilder();
 
-        key %= 26;
+        key %= SIZE;
         char ch;
         for (int i = 0; i < message.length(); i++) {
-            ch = Character.toLowerCase(message.charAt(i));
-            if ('a' <= ch && ch <= 'z') {
-                ch =  (char) ( 'a' + (ch - 'a' + key) % 26 );
-            }
-            strb.append(ch);
-        }
-        return strb.toString();
-    }
-
-    private static String encrypt(String message) {
-        StringBuilder strb = new StringBuilder();
-
-        char ch;
-        for (int i = 0; i < message.length(); i++) {
-            ch = Character.toLowerCase(message.charAt(i));
-            if ('a' <= ch && ch <= 'z') {
-                ch =  (char) ('a' + ((int) 'z') - ((int) ch));
-            }
+            ch =  message.charAt(i);
+            ch =  (char) ( 'a' + (ch - 'a' + key) % SIZE );
             strb.append(ch);
         }
         return strb.toString();
