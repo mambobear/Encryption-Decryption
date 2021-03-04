bb3cf70bd18d1e64fa1f044b4c951e0848ba7f94
Andrei Zherebtsov <azherebtsov@gmail.com>
2021-03-03 17:19:11 -0800

    Complete Stage 2

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -4,8 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "we found a treasure!";
-        System.out.println(encrypt(message));
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        int key = scanner.nextInt();
+        System.out.println(encryptCaesar(message, key));
+    }
+
+    private static String encryptCaesar(String message, int key) {
+        StringBuilder strb = new StringBuilder();
+
+        key %= 26;
+        char ch;
+        for (int i = 0; i < message.length(); i++) {
+            ch = Character.toLowerCase(message.charAt(i));
+            if ('a' <= ch && ch <= 'z') {
+                ch =  (char) ( 'a' + (ch - 'a' + key) % 26 );
+            }
+            strb.append(ch);
+        }
+        return strb.toString();
     }
 
     private static String encrypt(String message) {
